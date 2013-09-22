bad61be0e34d42b9b1d88dd23233a1544a46c498
Travis Jeffery <tj@travisjeffery.com>
2013-09-22 17:58:57 -0500

    Test -[UIColor trvs_colorWithHexString:]

--- TRVSKitTests/TRVSKitTests.m
+++ TRVSKitTests/TRVSKitTests.m
@@ -7,7 +7,9 @@
 //
 
 #import <XCTest/XCTest.h>
+// break these out into their own test files
 #import "NSString+TRVSKit.h"
+#import "UIColor+TRVSKit.h"
 
 @interface TRVSKitTests : XCTestCase
 
@@ -25,6 +27,12 @@
 
 - (void)testTrimmingString {
     XCTAssertEqualObjects(@"hello, world", [@"   hello, world\n  " trvs_stringByTrimmingWhitespace]);
+}
+
+- (void)testColorWithHexString {
+    UIColor *expected = [UIColor colorWithRed:236.f/255.f green:242.f/255.f blue:254.f/255.f alpha:1.f];
+    UIColor *actual = [UIColor trvs_colorWithHexString:@"ecf2fe"];
+    XCTAssertEqualObjects(actual, expected);
 }
 
 @end
