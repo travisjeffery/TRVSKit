f82462e137c4787e1b8e1202b823b10adf4a7df9
Travis Jeffery <tj@travisjeffery.com>
2014-05-26 01:52:27 -0500

    Add TRVSAssertions

---
+++ TRVSKit/TRVSAssertions.h
@@ -0,0 +1,9 @@
+//
+//  TRVSAssertions.h
+//  Tests
+//
+//  Created by Travis Jeffery on 5/26/14.
+//
+//
+
+void trvs_assertNotificationsObserved(id self, void (^block)(void), id firstNotification, ...);
---
+++ TRVSKit/TRVSAssertions.m
@@ -0,0 +1,36 @@
+//
+//  TRVSAssertions.c
+//  Tests
+//
+//  Created by Travis Jeffery on 5/26/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+void trvs_assertNotificationsObserved(id self, void (^block)(void), id firstNotification, ...) {
+    NSCParameterAssert(block != nil);
+    NSCParameterAssert(firstNotification != nil);
+    
+    va_list args;
+    va_start(args, firstNotification);
+    NSMutableArray *observers = [[NSMutableArray alloc] init];
+    __block NSUInteger actualObservedCount = 0;
+    NSUInteger expectedObservedCount = 0;
+    
+    for (id currentNotification = firstNotification; currentNotification != nil; currentNotification = va_arg(args, id)) {
+        ++expectedObservedCount;
+        
+        [observers addObject:[NSNotificationCenter.defaultCenter addObserverForName:currentNotification object:nil queue:nil usingBlock:^(NSNotification *note) {
+                              ++actualObservedCount;
+                              }]];
+    }
+    
+    va_end(args);
+    
+    block();
+    
+    expect(actualObservedCount).will.equal(expectedObservedCount);
+    
+    for (id observer in observers) [NSNotificationCenter.defaultCenter removeObserver:observer];
+}
